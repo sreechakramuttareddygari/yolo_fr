c2d64219a4fb269d856e172fd04db840504b662c
sreechakramuttareddygari <49017747+sreechakramuttareddygari@users.noreply.github.com>
2022-02-07 15:29:39 +0530

    Update prepare_data.sh

--- data/prepare_data.sh
+++ data/prepare_data.sh
@@ -36,31 +36,21 @@ get_file()
 
 echo "** Download dataset files"
 get_file https://drive.google.com/uc?id=1vHmWx7YLQ0ziByMt-c4wrxclNaJhEgqd Annotations_all.zip
-#https://drive.google.com/file/d/1vHmWx7YLQ0ziByMt-c4wrxclNaJhEgqd/view?usp=sharing
-#https://drive.google.com/file/d/1XFktSS2phiijh0YPVSTf80Zqo7_VIets/view?usp=sharing
-#https://drive.google.com/file/d/1GiTRhlUv22ejJF60ydcGh0Fw-Ex0uM5k/view?usp=sharing
-#https://drive.google.com/file/d/1ThkRW28OpuFbN3uOyt13q2k81Ll6LPsO/view?usp=sharing
-#https://drive.google.com/file/d/1kX-n2PQrcpY7r9d1L397QRyIcKGOON8o/view?usp=sharing
-#https://drive.google.com/file/d/1chnF6zgvfgPseqoa0rnhCLGruQYAjXJV/view?usp=sharing hisar entrance
-#https://drive.google.com/file/d/1ThkRW28OpuFbN3uOyt13q2k81Ll6LPsO/view?usp=sharing hissar queue
-#https://drive.google.com/file/d/1kX-n2PQrcpY7r9d1L397QRyIcKGOON8o/view?usp=sharing
-#https://drive.google.com/file/d/1rrBNaBdX_jJkFZD0qMa4DL1HU2FFlPx9/view?usp=sharing hissar entrance
-#https://drive.google.com/file/d/1kX-n2PQrcpY7r9d1L397QRyIcKGOON8o/view?usp=sharing
-#get_file https://drive.google.com/uc?id=1sQ9HWEk8o7cb0BHYfUzIeui3Mo8G4fwZ CrowdHuman_train02.zip
-#get_file https://drive.google.com/uc?id=1iFsJjMyYqgowEc8I5Y-z_ugX0NAvlLUy CrowdHuman_train03.zip
-#get_file https://drive.google.com/uc?id=1G8ASJYDkK32uie5lj1tyHFE9MriKHdhM CrowdHuman_val.zip
+get_file https://drive.google.com/uc?id=1sQ9HWEk8o7cb0BHYfUzIeui3Mo8G4fwZ CrowdHuman_train02.zip
+get_file https://drive.google.com/uc?id=1iFsJjMyYqgowEc8I5Y-z_ugX0NAvlLUy CrowdHuman_train03.zip
+get_file https://drive.google.com/uc?id=1G8ASJYDkK32uie5lj1tyHFE9MriKHdhM CrowdHuman_val.zip
 # test data is not needed...
-# get_file https://drive.google.com/uc?id=1tQG3E_RrRI4wIGskorLTmDiWHH2okVvk CrowdHuman_test.zip
-#get_file https://drive.google.com/u/0/uc?id=1MX5jVfUNm-VC8OCg6YKzwwe7nd4PomUf annotation_train.odgt
-#get_file https://drive.google.com/u/0/uc?id=103SIb64U-llIHoTUnwAuwi9e41Ny_tnD annotation_val.odgt
+#get_file https://drive.google.com/uc?id=1tQG3E_RrRI4wIGskorLTmDiWHH2okVvk CrowdHuman_test.zip
+get_file https://drive.google.com/u/0/uc?id=1MX5jVfUNm-VC8OCg6YKzwwe7nd4PomUf annotation_train.odgt
+get_file https://drive.google.com/u/0/uc?id=103SIb64U-llIHoTUnwAuwi9e41Ny_tnD annotation_val.odgt
 
 # unzip image files (ignore CrowdHuman_test.zip for now)
 echo "** Unzip dataset files"
-#for f in CrowdHuman_train01.zip CrowdHuman_train02.zip CrowdHuman_train03.zip CrowdHuman_val.zip ; do
-#  unzip -n ${f}
-#done
+for f in CrowdHuman_train01.zip CrowdHuman_train02.zip CrowdHuman_train03.zip CrowdHuman_val.zip ; do
+  unzip -n ${f}
+done
 
-unzip -n queue_cons_yolo_format_person2.zip
+#unzip -n queue_cons_yolo_format_person2.zip
 echo "** Create the crowdhuman-$1/ subdirectory"
 rm -rf ../crowdhuman-$1/
 mkdir ../crowdhuman-$1/
