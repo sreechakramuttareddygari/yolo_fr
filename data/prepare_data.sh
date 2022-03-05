4238b9b35e5699e3c575c2f1298d1ed7b4968524
sreechakramuttareddygari <49017747+sreechakramuttareddygari@users.noreply.github.com>
2022-03-05 10:47:33 +0530

    Update prepare_data.sh

--- data/prepare_data.sh
+++ data/prepare_data.sh
@@ -35,7 +35,7 @@ get_file()
 }
 
 echo "** Download dataset files"
-get_file https://drive.google.com/uc?id=1SxQR_VQ5_YVrx4xV5TNUJYxtZnvrSfDU Annotations_all.zip
+get_file https://drive.google.com/uc?id=1MNDgrlO3oFWymMWFfmfg_P2RvdlFK8w8 Annotations_all.zip
 #https://drive.google.com/file/d/1SxQR_VQ5_YVrx4xV5TNUJYxtZnvrSfDU/view?usp=sharing
 #https://drive.google.com/file/d/1vkxbYmpQMqDpO1qt7Laid72nuF0JPUA_/view?usp=sharing
 #https://drive.google.com/file/d/1vkxbYmpQMqDpO1qt7Laid72nuF0JPUA_/view?usp=sharing
@@ -53,12 +53,12 @@ echo "** Unzip dataset files"
 #  unzip -n ${f}
 #done
 
-unzip -n annotation_check_yolo_format_person.zip
+unzip -n inout_mumbai_entrance_dataset_yolo_format_person.zip
 echo "** Create the crowdhuman-$1/ subdirectory"
 rm -rf ../crowdhuman-$1/
 mkdir ../crowdhuman-$1/
-ln annotation_check_yolo_format_person/*.jpg ../crowdhuman-$1/
-ln annotation_check_yolo_format_person/*.txt ../crowdhuman-$1/
+ln inout_mumbai_entrance_dataset_yolo_format_person/*.jpg ../crowdhuman-$1/
+ln inout_mumbai_entrance_dataset_yolo_format_person/*.txt ../crowdhuman-$1/
 
 # the crowdhuman/ subdirectory now contains all train/val jpg images
 
